8257ddc44fdba8702067858dbba6aaec199e00ef
Denis.Suchan <129043212+Dennisex@users.noreply.github.com>
2024-07-29 10:43:01 +0200

    first commit

---
+++ src/main/java/com/goit/GoIT2_HW_16/GoIt2Hw16Application.java
@@ -0,0 +1,13 @@
+package com.goit.GoIT2_HW_16;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoIt2Hw16Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoIt2Hw16Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/goit/GoIT2_HW_16/GoIt2Hw16ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goit.GoIT2_HW_16;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoIt2Hw16ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
